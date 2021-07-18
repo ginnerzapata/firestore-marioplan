20c142abe0365498c9da583c8746bb85876a3ea2
Ginner Zapata <gzapata@vizzn.ca>
2021-07-18 19:43:16 -0300

    created createProject structure

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Dashboard from './components/dashboard/Dashboard'
 import ProjectDetails from './components/projects/ProjectDetails'
 import SignIn from './components/auth/SignIn'
 import SignUp from './components/auth/SignUp'
+import CreateProject from './components/projects/CreateProject'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Navbar />
         <Switch>
           <Route path="/" exact sicomponent={Dashboard} />
+          <Route path="/create" component={CreateProject} />
           <Route path="/project/:id" component={ProjectDetails} />
           <Route path="/signin" component={SignIn} />
           <Route path="/signup" component={SignUp} />
