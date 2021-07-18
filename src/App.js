1c9eb4565dd22bf332dff0eff42aa87b15eb7759
Ginner Zapata <gzapata@vizzn.ca>
2021-07-18 19:35:11 -0300

    created signin and signup component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Dashboard from './components/dashboard/Dashboard'
 import ProjectDetails from './components/projects/ProjectDetails'
+import SignIn from './components/auth/SignIn'
+import SignUp from './components/auth/SignUp'
 
 function App() {
   return (
@@ -10,8 +12,10 @@ function App() {
       <div className="App">
         <Navbar />
         <Switch>
-          <Route path="/" exact component={Dashboard} />
+          <Route path="/" exact sicomponent={Dashboard} />
           <Route path="/project/:id" component={ProjectDetails} />
+          <Route path="/signin" component={SignIn} />
+          <Route path="/signup" component={SignUp} />
         </Switch>
       </div>
     </BrowserRouter>
